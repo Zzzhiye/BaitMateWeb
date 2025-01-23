2dd502859519e84213718e1749fe5ca7645e012e
zhiye <zhiye_yuhua@yahoo.com.sg>
2025-01-23 17:10:20 +0800

    first commit

---
+++ src/test/java/baitmate/BaitMateWebApplicationTests.java
@@ -0,0 +1,13 @@
+package baitmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaitMateWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
