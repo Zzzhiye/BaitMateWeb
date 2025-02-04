0c2a9d57bb89355127ddd77f33285396d183519c
hannah <hannah.wong@u.nus.edu>
2025-02-04 11:06:32 +0800

    enable scheduling

--- src/main/java/baitmate/BaitMateWebApplication.java
+++ src/main/java/baitmate/BaitMateWebApplication.java
@@ -2,8 +2,10 @@ package baitmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BaitMateWebApplication {
 
 	public static void main(String[] args) {
