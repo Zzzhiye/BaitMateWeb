f875ccfaaa75eebf2d750c0db4dc9240e16762bd
hannah <hannah.wong@u.nus.edu>
2025-02-09 15:08:46 +0800

    enable https via port 8443 (http still allowed via 8080)

--- src/main/java/baitmate/config/SecurityConfig.java
+++ src/main/java/baitmate/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package baitmate.config;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -24,5 +27,14 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> customizer() {
+        return factory -> {
+            Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+            connector.setPort(8080); // HTTP port
+            factory.addAdditionalTomcatConnectors(connector);
+        };
     }
 }
